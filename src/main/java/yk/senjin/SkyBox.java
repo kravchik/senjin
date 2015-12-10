bd93ada149b5c0cf59117789528437965347999a
kravchik <mindwarp@list.ru>
2015-12-10 18:07:26 +0200

    + SkyBox rotation - SkyBox texture bug

--- src/main/java/yk/senjin/SkyBox.java
+++ src/main/java/yk/senjin/SkyBox.java
@@ -1,12 +1,14 @@
 package yk.senjin;
 
 import yk.jcommon.collections.YList;
+import yk.jcommon.fastgeom.Quaternionf;
 import yk.jcommon.fastgeom.Vec3f;
 
 import java.awt.image.BufferedImage;
 
 import static org.lwjgl.opengl.GL11.*;
 import static yk.jcommon.collections.YArrayList.al;
+import static yk.senjin.DDDUtils.glMultMatrix4;
 import static yk.senjin.DDDUtils.glVec3f;
 
 /**
@@ -20,6 +22,9 @@ public class SkyBox {
     private SomeTexture st;
     private YList<SomeTexture> st2;
 
+    public Quaternionf rot;
+    public boolean enableTexture = true;
+
     public void firstFrame(BufferedImage image) {
         st = new SomeTexture(image);
     }
@@ -32,7 +37,11 @@ public class SkyBox {
         st2.add(new SomeTexture(getSubImage(images, 3, 1)));
         st2.add(new SomeTexture(getSubImage(images, 1, 0)));
         st2.add(new SomeTexture(getSubImage(images, 1, 2)));
+    }
 
+    public void firstFrame3(YList<BufferedImage> images) {
+        st2 = al();
+        for (BufferedImage img : images) st2.add(new SomeTexture(img));
     }
 
     private BufferedImage getSubImage(BufferedImage images, int px, int py) {
@@ -48,8 +57,8 @@ public class SkyBox {
         else render2(camPos);
     }
 
-    private static void renderQuad(SomeTexture st, Vec3f pos, Vec3f vx, Vec3f vy) {
-        st.enable();
+    private void renderQuad(SomeTexture st, Vec3f pos, Vec3f vx, Vec3f vy) {
+        if (enableTexture) st.enable(0);
 
         glBegin(GL_QUADS);
         glColor3f(1, 1, 1);
@@ -72,13 +81,14 @@ public class SkyBox {
 //
 //        glEnd();
 
-        st.disable();
+        if (enableTexture) st.disable();
     }
 
     private void render2(Vec3f camPos) {
         glMatrixMode(GL_MODELVIEW);
         glPushMatrix();
         glTranslatef(camPos.x, camPos.y, camPos.z);
+        if (rot != null) glMultMatrix4(rot.toMatrix4());
 
         renderQuad(st2.get(0), new Vec3f(-10, -10,  10), new Vec3f(  0, 0,-20), new Vec3f(0, 20,  0));
         renderQuad(st2.get(1), new Vec3f(-10, -10, -10), new Vec3f( 20, 0,  0), new Vec3f(0, 20,  0));
