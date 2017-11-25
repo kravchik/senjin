12d9064fc63ec43af7064867aa31aa6fe0444893
ykravchik <ifd83jw823>
2017-11-25 12:59:07 +0200

    *

--- src/main/java/yk/senjin/examples/simple/IKnowOpenGL_5_PixelPerfect.java
+++ src/main/java/yk/senjin/examples/simple/IKnowOpenGL_5_PixelPerfect.java
@@ -25,6 +25,7 @@ public class IKnowOpenGL_5_PixelPerfect extends SimpleLwjglRoutine {
         glLoadMatrix(ortho(0, w, 0, h, 0, 10));//0,0 is LEFT BOTTOM
 
         glBegin(GL_TRIANGLES);
+        glColor3f(1, 1, 1);
         glVertex3f(0, 0, 0);
         glVertex3f(10, 0, 0);
         glVertex3f(10, 10, 0);
@@ -46,9 +47,18 @@ public class IKnowOpenGL_5_PixelPerfect extends SimpleLwjglRoutine {
         glVertex3f(0, 0, 0);
         glVertex3f(w - 1, h - 1, 0);
 
-        //line is drawn one pixel short! wtf??? (try to switch those two lines)
-        glVertex3f(0, h - 1, 0);
         glVertex3f(w - 1, 0, 0);
+        glVertex3f(0, h - 1, 0);
+
+        //line is drawn one pixel short! [from, to)
+        glVertex3f(23, 10, 0);glVertex3f(30, 10, 0);
+        glColor3f(1, 0, 0);
+        glVertex3f(24, 11, 0);glVertex3f(24, 11, 0);//can't see (one pixel length)
+        glVertex3f(25, 11, 0);glVertex3f(25, 12, 0);//one pixel only
+        glVertex3f(26, 11, 0);glVertex3f(26, 13, 0);//2 pixel
+
+
+
         glEnd();
 
         glBegin(GL_POINTS);
